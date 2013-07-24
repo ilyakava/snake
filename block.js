2a25d29cca870b54da3c75b0428c7677292d6f83
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-07-24 16:38:52 -0400

    Refactor Snake movement method to add head to tail, instead of moving whole body

--- block.js
+++ block.js
@@ -53,7 +53,7 @@ var Snake = function () {
 Snake.spawn = function (xCenter, yCenter) {
   var initSnake = new Snake();
 
-  US.times(13, function(i) {
+  US.times(23, function(i) {
     initSnake.body.push(new Block(xCenter + i, yCenter));
   });
   return initSnake;
@@ -61,18 +61,9 @@ Snake.spawn = function (xCenter, yCenter) {
 
 Snake.prototype.move = function () {
   var that = this;
-  if (that.apex < that.body.length) {
-    var front = US.first(that.body, that.apex);
-    front = US.map(front, function(unit) { return unit.add(that.facDir); });
-
-    var back = US.rest(that.body, that.apex);
-    back = US.map(back, function(unit) { return unit.add(that.oldDir); });
-
-    this.body = front.concat(back);
-    this.apex += 1;
-  } else {
-    this.body = US.map(that.body, function(unit) { return unit.add(that.facDir); });
-  }
+  that.body.pop();
+  // adds head to the tail
+  this.body = [US.first(that.body).copy().add(that.facDir)].concat(that.body);
 };
 
 Snake.prototype.turn = function(direction) {
