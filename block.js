7c1da7550b6c19bfb53f3b8e59329e494bc258d6
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-07-24 11:48:22 -0400

    Add turn basic functionality

--- block.js
+++ block.js
@@ -12,9 +12,33 @@ Block.prototype.add = function (block2) {
   return new Block(that.x + block2.x, that.y + block2.y);
 };
 
+Block.prototype.left = function () {
+  var tmpY = this.y;
+  if (this.y === 0) {
+    this.y = -this.x;
+    this.x = tmpY;
+  } else {
+    this.y = this.x;
+    this.x = tmpY;
+  }
+};
+
+Block.prototype.right = function () {
+  var tmpY = this.y;
+  if (this.x === 0) {
+    this.y = this.x;
+    this.x = -tmpY;
+  } else {
+    this.y = this.x;
+    this.x = tmpY;
+  }
+};
+
 var Snake = function () {
-  this.orientation = new Block(-1, 0);
+  this.facDir = new Block(-1, 0);
   this.body = [];
+  this.oldDir = null;
+  this.apex = NaN;
 };
 
 Snake.spawn = function (xCenter, yCenter) {
@@ -28,7 +52,28 @@ Snake.spawn = function (xCenter, yCenter) {
 
 Snake.prototype.move = function () {
   var that = this;
-  this.body = US.map(that.body, function(unit) { return unit.add(that.orientation); });
+  if (that.apex < that.body.length) {
+    var front = US.first(that.body, that.apex);
+    front = US.map(front, function(unit) { return unit.add(that.facDir); });
+
+    var back = US.rest(that.body, that.apex);
+    back = US.map(back, function(unit) { return unit.add(that.oldDir); });
+
+    this.body = front.concat(back);
+    this.apex += 1;
+  } else {
+    this.body = US.map(that.body, function(unit) { return unit.add(that.facDir); });
+  }
+};
+
+Snake.prototype.turn = function(direction) {
+  this.oldDir = this.facDir;
+  if (direction == "left") {
+    this.facDir.left();
+  } else {
+    this.facDir.right();
+  }
+  this.apex = 1;
 };
 
 var Board = function (xBoard, yBoard) {
